e82898ebc84feb525d6afb75efa23598b696515a
jbfink <john.fink@gmail.com>
2019-11-02 17:29:04 -0400

    Sentiment analysis works now.

--- sentiment.py
+++ sentiment.py
@@ -3,5 +3,7 @@ from textblob import TextBlob
 print("The file you selected is:", sys.argv[1])
 text_file = open(sys.argv[1],"r")
 output = text_file.read()
-print(output)
+#print(output)
+text_sentiment = TextBlob(output)
+print(text_sentiment.sentiment)
 print("I'M PROGRAMMING")

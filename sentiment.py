0b60b07d027780b0e849a393e650a617b30f71a7
jbfink <john.fink@gmail.com>
2019-11-05 16:11:26 -0500

    Some comments and whitespace.

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,13 @@
 import sys
 from textblob import TextBlob
+
 print("The file you selected is:", sys.argv[1])
 text_file = open(sys.argv[1],"r")
 output = text_file.read()
 text_sentiment = TextBlob(output)
 score = text_sentiment.sentiment.polarity
 print(text_sentiment.sentiment)
+# If/else tree for judging sentiment
 if score > 0.01 and score < 0.3:
     print("I declare this.... somewhat positive!") 
 elif score > 0.3:

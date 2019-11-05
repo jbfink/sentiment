35c024f7890e4eb452cf75648c0c41e64cb339e8
jbfink <john.fink@gmail.com>
2019-11-05 15:55:20 -0500

    Assigning polarity to its own variable for later use.

--- sentiment.py
+++ sentiment.py
@@ -4,4 +4,5 @@ print("The file you selected is:", sys.argv[1])
 text_file = open(sys.argv[1],"r")
 output = text_file.read()
 text_sentiment = TextBlob(output)
+score = text_sentiment.sentiment.polarity
 print(text_sentiment.sentiment)

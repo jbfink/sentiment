640a4314a2c96b7f61cba1579ec22811b6187dfc
jbfink <john.fink@gmail.com>
2019-11-06 09:25:30 -0500

    This is broken; will fix.

--- sentiment.py
+++ sentiment.py
@@ -4,17 +4,6 @@ from textblob import TextBlob
 # turning the judge into a function
 def judge(score):
     "This function takes the sentiment score and applies a judgment to it"
-    print(score)
-    return
-
-
-print("The file you selected is:", sys.argv[1])
-text_file = open(sys.argv[1],"r")
-output = text_file.read()
-text_sentiment = TextBlob(output)
-score = text_sentiment.sentiment.polarity
-print(text_sentiment.sentiment)
-# If/else tree for judging sentiment
 if score > 0.01 and score < 0.3:
     print("I declare this.... somewhat positive!") 
 elif score > 0.3:
@@ -28,3 +17,11 @@ elif score == 0:
 else:
     print("wtf!!!!??")
 
+print("The file you selected is:", sys.argv[1])
+text_file = open(sys.argv[1],"r")
+output = text_file.read()
+text_sentiment = TextBlob(output)
+score = text_sentiment.sentiment.polarity
+print(text_sentiment.sentiment)
+# If/else tree for judging sentiment
+judge(score)
